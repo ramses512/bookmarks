30fe7ff9c50c9e9382685bc1b6b1100eba0743ae
Ramsés Fernández <aramsesfernandez@gmail.com>
2020-08-07 14:13:40 +0200

    feat: create component add

---
+++ src/app/bookmarks/add-dialog/add-dialog.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, Inject, NgZone } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Bookmark } from '../bookmark.interface';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-add-dialog',
+  templateUrl: './add-dialog.component.html',
+  styleUrls: ['./add-dialog.component.scss'],
+})
+export class AddDialogComponent implements OnInit {
+  bookmarkForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    url: new FormControl('', Validators.required),
+    group: new FormControl('', Validators.required),
+  });
+  constructor(
+    public dialogRef: MatDialogRef<AddDialogComponent>,
+    private ngZone: NgZone,
+    @Inject(MAT_DIALOG_DATA) public data: Bookmark
+  ) {}
+
+  ngOnInit(): void {}
+  public save(): void {
+    if (this.bookmarkForm.valid) {
+      console.log(this.bookmarkForm.value);
+      this.ngZone.run(() => {
+        this.dialogRef.close(this.bookmarkForm.value);
+      });
+    }
+  }
+}
