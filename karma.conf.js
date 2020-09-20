852354401c5c7c21e02f066afdb4bc979ac44b18
Ramses Fernández <aramsesfernandez@gmail.com>
2020-09-20 12:23:48 +0200

    refactor: change name project

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/interview-accenture'),
+      dir: require('path').join(__dirname, './coverage/bookmarks'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
